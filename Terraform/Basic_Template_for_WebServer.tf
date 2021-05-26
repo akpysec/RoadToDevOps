4d8c5dfa6cfa9f421eea0e5e40100c23ee8249dd
AKPySec <48283299+akpysec@users.noreply.github.com>
2021-05-26 12:55:47 +0300

    Update Basic_Template_for_WebServer.tf

--- Terraform/Basic_Template_for_WebServer.tf
+++ Terraform/Basic_Template_for_WebServer.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIATB4WPNMF4H4YPQUG"
-  secret_key = "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"
+  access_key = "ACCESS_KEY"
+  secret_key = "SECRET_KEY"
 }
 
 # VPC
