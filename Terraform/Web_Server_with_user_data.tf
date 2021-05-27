08fbc28af8a9f931780d8a8bb5d98ca2bfd920b4
AKPySec <48283299+akpysec@users.noreply.github.com>
2021-05-27 20:29:47 +0300

    Update Web_Server_with_user_data.tf

--- Terraform/Web_Server_with_user_data.tf
+++ Terraform/Web_Server_with_user_data.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web_server" {
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web_server_security_group.id]
-  user_data              = file("user_data.sh")
+  user_data              = file("./Scripts/user_data_1.sh")
 
   tags = {
     Name = "Terraform Web Server"
